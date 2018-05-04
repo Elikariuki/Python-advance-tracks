296d4f2184cf1e7a5334dea4c14b5074526f9eba
elikariuki <ngugidoc4000@gmail.com>
2018-05-04 15:00:30 -0800

    finished multiples sum

--- koan_labs/multiples.py
+++ koan_labs/multiples.py
@@ -1,4 +1,8 @@
 def multiple_sum():
-	#TODO: Your code goes here
-	return
+	#TODO: Your code goes 
+	numbers = []
+	for i in range(1, 1000):
+		if i % 3 == 0 or i % 5 == 0:
+			numbers.append(i)
+	return sum(numbers)
 	
